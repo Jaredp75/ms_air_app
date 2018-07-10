4e6e98d50e94c3b905666300531702c3d4171052
Jared Patterson <Jaredp75@hotmil.com>
2018-07-09 22:52:44 -0400

    Product page & styling updates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import prodID8 from './components/product_pages/prodID8';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={prodID8} />
+    </Switch>
+  </BrowserRouter>
+  , document.getElementById('root'));
 registerServiceWorker();
