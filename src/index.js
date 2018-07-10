f9276495fab05646189f83bce9a05f7ed949777f
Jared Patterson <Jaredp75@hotmil.com>
2018-07-10 12:09:22 -0400

    About page updates

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import Home from './components/Home.js';
+import About from './components/About.js';
 import prodID8 from './components/product_pages/prodID8';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
       <Route path="/components/product_pages/prodID8" component={prodID8} />
     </Switch>
   </BrowserRouter>
