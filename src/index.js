dbb4a353e75e44f2288b426ff83994add11bf22d
Jared Patterson <Jaredp75@hotmil.com>
2018-07-10 11:55:57 -0400

    Styling Updates

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Home from './components/Home.js';
 import prodID8 from './components/product_pages/prodID8';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={prodID8} />
+      <Route exact path="/" component={Home} />
+      <Route path="/components/product_pages/prodID8" component={prodID8} />
     </Switch>
   </BrowserRouter>
   , document.getElementById('root'));
